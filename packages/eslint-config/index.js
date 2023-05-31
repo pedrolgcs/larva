ef1b0080ce4e0aba5c79f0cc5b6cc2f0ab611543
Pedro Henrique <pedro.lg.cs@gmail.com>
2023-05-31 12:59:01 -0300

    chore: configure eslint import helper

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,3 +1,26 @@
 module.exports = {
   extends: '@rocketseat/eslint-config/react',
+  plugins: ['eslint-plugin-import-helpers'],
+  rules: {
+    'no-case-declarations': 'off',
+    camelcase: 'off',
+    'import-helpers/order-imports': [
+      'warn',
+      {
+        newlinesBetween: 'never',
+        groups: [
+          '/^react$/',
+          '/^next/',
+          '/^@[a-zA-Z]/',
+          'module',
+          '/^@/',
+          ['parent', 'sibling', 'index'],
+        ],
+        alphabetize: {
+          order: 'asc',
+          ignoreCase: true,
+        },
+      },
+    ],
+  },
 };
