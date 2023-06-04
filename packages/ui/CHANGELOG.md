76934d1cced1476d8437deec46fcf80dccd19c5d
Pedro Henrique <pedro.lg.cs@gmail.com>
2023-06-04 14:01:44 -0300

    feature: export masks

--- packages/ui/CHANGELOG.md
+++ packages/ui/CHANGELOG.md
@@ -1,5 +1,11 @@
 # @ubli/ui
 
+## 1.1.0
+
+### Minor Changes
+
+- export masks
+
 ## 1.0.0
 
 ### Major Changes
--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -1,3 +1,3 @@
 export * from './components'
 export * from './functions'
-export * from './masks'
+export * as masks from './masks'
